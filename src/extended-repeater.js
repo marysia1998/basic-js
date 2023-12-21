303d86fb5174c7632e81b788252f7b9ffd8a832c
unknown <m.n.volkova22@gmail.com>
2023-12-21 14:40:33 +0300

    feat:add extended repeater func

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,22 +2,49 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let arrAddition = [];
+  for(let i =0; i<options.additionRepeatTimes; i++) {
+    arrAddition.push(options.addition+"")
+  }
+  console.log(arrAddition)
+  let strAddition = ""
+  if(options.additionSeparator) {
+    strAddition = arrAddition.join(options.additionSeparator);
+  } else {
+    strAddition = arrAddition.join("|");
+  }
+
+  if(!options.additionRepeatTimes && options.addition) {
+    strAddition = options.addition
+}
+  let arrStr = [];
+  for(let i =0; i<options.repeatTimes; i++) {
+    arrStr.push(str + strAddition)
+  }
+  if(!options.repeatTimes && !options.additionRepeatTimes){
+    return str + options.addition;
+  }
+
+  if(options.separator) {
+    return arrStr.join(options.separator)
+  } else {
+    return arrStr.join("+");
+  }
+
 }
 
 module.exports = {
